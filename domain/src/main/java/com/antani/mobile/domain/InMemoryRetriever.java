3fca23b3d7add152aa032c03ab77c5debee15f41
Albert Minetti <alberto.minetti@rumbo.com>
2016-03-03 15:28:14 +0100

    implement the InMemoryRetriver

--- domain/src/main/java/com/antani/mobile/domain/InMemoryRetriever.java
+++ domain/src/main/java/com/antani/mobile/domain/InMemoryRetriever.java
@@ -1,13 +1,21 @@
 package com.antani.mobile.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by fgiovinazzo on 03/03/16.
  */
 public class InMemoryRetriever implements Retriever {
+
+    private List<String> courses;
+
     @Override
     public List<String> retrieveCourse() {
-        return null;
+        return courses;
+    }
+
+    public InMemoryRetriever(List<String> courses) {
+        this.courses = courses;
     }
 }
